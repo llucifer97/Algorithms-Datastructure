4f1af21eb65f8e527033a1770f2d32ebb0631fa9
ayush raj <ayushrajpoot97@gmail.com>
2020-03-27 22:44:33 +0530

    Add files via upload

--- graph algorithm/stronglyconnectedcomponents.cpp
+++ graph algorithm/stronglyconnectedcomponents.cpp
@@ -1,57 +1,75 @@
-//strongly connected component in undirected graph
+#include<bits/stdc++.h>
+using namespace std;
 
-#include <bits/stdc++.h>
-using namespace std;              
-
-map<int,list<int> > adjList;
-
-bool visited[99999] = {false};
-class Graph{
-public:
-    Graph(){
-    }
-    
-    void addEdge(int u, int v,bool bidir=true){
-        adjList[u].push_back(v);
-        if(bidir){
-        adjList[v].push_back(u);
+vector<int>graph[10005];
+vector<int>revgraph[10005];
+stack<int> s;
+bool mrk[10005];
+void dfs(int src)
+{
+    mrk[src] = true;
+    for(int i = 0 ; i < graph[src].size() ;i++)
+    {
+        if(!mrk[graph[src][i]])
+        {
+            dfs(graph[src][i]);
         }
-        
     }
-			
-		void dfs(int v) {
-			visited[v] = true;
-			for (int u : adjList[v]) {
-				if (!visited[u])
-					dfs(u);
-			}
-			cout << v << "-->";
-		}
+    s.push(src);
+}
+void dfs2(int src)
+{
+mrk[src] = true;
+cout<<src<<"-";
+for(int i = 0 ; i< revgraph[src].size(); i++)
+{
+    if(!mrk[revgraph[src][i]])
+    {
+        dfs2(revgraph[src][i]);
+    }
+}
+}
+int main()
+{
+    int v;
+    cin >> v;
+    int e;
+    cin >> e;
+    int x,y;
+    int i,j;
+    for(i = 0 ; i < e ; i++)
+    {
+        cin >> x >> y;
+        graph[x].push_back(y);
+        revgraph[y].push_back(x);
+    }
+    for(i = 0 ; i < v; i++)
+    {
+        mrk[i] = false;
+    }
+    for(i = 0 ; i < v; i++)
+    {
+        if(!mrk[i])
+        {
+            dfs(i);
+        }
+    }
+    //NOW reversing graph's dfs;
+    for(i = 0 ; i < v ; i++)
+    {
+        mrk[i] = false;
+    }
+    cout<<"CONNECTED COMPONENTS ARE\n";
+    while(!s.empty())
+    {
+        int tmp = s.top();
+        s.pop();
+        if(!mrk[tmp])
+        {
+            dfs2(tmp);
+            cout<<endl;
+        }
+    }
 
-   
-    
-    
-    
- };
- 
- 
- 
- 
-
-int main(){
-Graph g;
-g.addEdge(1,0,true);
-g.addEdge(1,2,true);
-g.addEdge(3,4,true);
-
-for(int i =0;i<5;i++){
-
-	if(!visited[i]){
-		
-		g.dfs(i);
-		cout << "\n";
-		}
-	
-	}
-return 0;
+    return 0;
 }
