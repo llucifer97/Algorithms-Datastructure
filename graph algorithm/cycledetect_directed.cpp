a71c182ad35d05b67f18bda8f608b16e2ba71e49
ayush raj <ayushrajpoot97@gmail.com>
2020-04-11 20:55:44 +0530

    Update cycledetect_directed.cpp

--- graph algorithm/cycledetect_directed.cpp
+++ graph algorithm/cycledetect_directed.cpp
@@ -4,15 +4,11 @@
 #include<queue>
 #include<bits/stdc++.h> 
 using namespace std;
-template<typename T>
-class Graph{
+
     int V;
-map<T,list<T> > adjList;
-public:
-    Graph(int v){
-        V = v;
-    }
-    void addEdge(T u, T v,bool bidir=true){
+map<int,list<int> > adjList;
+
+    void addEdge(int u, int v,bool bidir=true){
         adjList[u].push_back(v);
         if(bidir){
         adjList[v].push_back(u);
@@ -20,12 +16,12 @@ public:
         
     }
    
-    bool isCyclicHelper(T node,map<T,bool> &visited,map<T,bool> &instack){
+    bool isCyclicHelper(int node,map<int,bool> &visited,map<int,bool> &instack){
         
         visited[node] = true;
         instack[node] = true;
         //Explore the neighbours of the node
-        for(T neighbour:adjList[node]){
+        for(int neighbour:adjList[node]){
 			//two things can happen
 			//the current node is not visied but its further branch is leading to a cycle
             if((!visited[neighbour] && isCyclicHelper(neighbour,visited,instack))||instack[neighbour]){
@@ -40,11 +36,11 @@ public:
     
     
     bool isCyclic(){
-        map<T,bool> visited;
-        map<T,bool> instack;
+        map<int,bool> visited;
+        map<int,bool> instack;
         // you can for finding in every dfs tree
         for(auto i:adjList){
-            T node = i.first;
+            int node = i.first;
             if(!visited[node]){
                 bool cyclePresent = isCyclicHelper(node,visited,instack);
                 if(cyclePresent == true){
@@ -55,17 +51,16 @@ public:
         return false;
     }
     
-};
+
 int main(){
-Graph<int> g(4);
-g.addEdge(0,2,false);
-g.addEdge(0,1,false);
-g.addEdge(2 ,3,false);
-g.addEdge(2,4,false);
-g.addEdge(3,0,false);
-g.addEdge(4,5,false);
-g.addEdge(1,5,false);
-if(g.isCyclic()){
+addEdge(0,2,false);
+addEdge(0,1,false);
+addEdge(2 ,3,false);
+addEdge(2,4,false);
+addEdge(3,0,false);
+addEdge(4,5,false);
+addEdge(1,5,false);
+if(isCyclic()){
     cout << "cyclic graph";
 }
 else{
