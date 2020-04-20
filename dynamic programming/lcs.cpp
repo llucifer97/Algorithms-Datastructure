016e92946abc16e2551166078ed7f3b44769ec23
ayush raj <ayushrajpoot97@gmail.com>
2020-04-20 14:42:28 +0530

    Update lcs.cpp

--- dynamic programming/lcs.cpp
+++ dynamic programming/lcs.cpp
@@ -70,15 +70,15 @@ int lcs_memoization(string X, string Y, int m, int n, int dp[][maximum])
 int lcs_bu( string X, string Y, int m, int n )  
 {  
     int L[m + 1][n + 1];  
-    int i, j;  
+   // int i, j;  
       
     /* Following steps build L[m+1][n+1] in  
        bottom up fashion. Note that L[i][j]  
        contains length of LCS of X[0..i-1] 
        and Y[0..j-1] */
-    for (i = 0; i <= m; i++)  
+    for (int i = 0; i <= m; i++)  
     {  
-        for (j = 0; j <= n; j++)  
+        for (int j = 0; j <= n; j++)  
         {  
         if (i == 0 || j == 0)  
             L[i][j] = 0;  
@@ -93,6 +93,37 @@ int lcs_bu( string X, string Y, int m, int n )
           
     /* L[m][n] contains length of LCS  
     for X[0..n-1] and Y[0..m-1] */
+  ///////////////////////////////////////////////////////////////////  
+     // Following code is used to print LCS 
+   int index = L[m][n]; 
+  
+   // Create a character array to store the lcs string 
+   char lcs[index+1]; 
+   lcs[index] = '\0'; // Set the terminating character 
+  
+   // Start from the right-most-bottom-most corner and 
+   // one by one store characters in lcs[] 
+   int i = m, j = n; 
+   while (i > 0 && j > 0) 
+   { 
+      // If current character in X[] and Y are same, then 
+      // current character is part of LCS 
+      if (X[i-1] == Y[j-1]) 
+      { 
+          lcs[index-1] = X[i-1]; // Put current character in result 
+          i--; j--; index--;     // reduce values of i, j and index 
+      } 
+  
+      // If not same, then find the larger of two and 
+      // go in the direction of larger value 
+      else if (L[i-1][j] > L[i][j-1]) 
+         i--; 
+      else
+         j--; 
+   } 
+  
+   // Print the lcs 
+   cout << "LCS of " << X << " and " << Y << " is " << lcs; 
     return L[m][n];  
 }  
 
@@ -151,9 +182,9 @@ int main()
     // assign -1 to all positions 
     memset(dp, -1, sizeof(dp)); 
     
-   cout << lcs_recursive(s1,s2,m,n);
+  // cout << lcs_recursive(s1,s2,m,n);
    cout << lcs_bu(s1,s2,m,n);
-  cout <<  lcs_space_optimised(s1,s2);
+ // cout <<  lcs_space_optimised(s1,s2);
     
    //cout<< lcs_memoization(s1,s2,m,n,dp);
   return 0;
