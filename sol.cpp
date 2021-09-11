94571c5e873e2384c300cb77041b29697975ecaf
ayush raj <ayushraj.bit17@gmail.com>
2021-09-11 13:59:44 +0530

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -1,24 +1,23 @@
 
 
-bool coprime(int a, int b) {
-     
-    if ( __gcd(a, b) == 1)
-        return true;
-    else
-        return false;    
-}
-
-
-
-
 int find(int input1[],int input2)
 {
 	for (int i = 0; i < input2 - 1 ; i++ )
 	{
-		if(coprime(input1[i],input1[i+1]) && (input1[i] > input1[i+1] ))
-		{
-			return i;
-		}
+		
+
+		bool flag1 = false,flag2 = false;
+
+		 if ( __gcd(input1[i],input1[i+1]) == 1)flag1 = true;
+
+		 if (input1[i] > input1[i+1]) flag2 = true;
+
+		 if(flag1 && flag2)
+		 {
+			 return i;
+		 }
+
+
 	}
 return -1;
 }
